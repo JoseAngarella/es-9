70b754edbfa6fa0af6bff36993f694723b2153f0
Benncio09 <lorebencini13@outlook.com>
2024-09-27 08:41:59 +0200

    esercizio finito e funzionante

--- esercizio9.php
+++ esercizio9.php
@@ -6,6 +6,16 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+        $nome="Jose Angarella Crispo";
+        $r=rand(0,255);
+        $g=rand(0,255);
+        $b=rand(0,255);
+
+        $str_nome= "<h1 style='color:rgb($r,$g,$b)'>$nome</h1>";
+        echo $str_nome;
+
+    ?>
     
 </body>
 </html>
